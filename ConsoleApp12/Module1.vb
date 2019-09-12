bc23b4c8ccd034f04a0e85991aedf3050d5b280b
nishtha-gupta03 <54981711+nishtha-gupta03@users.noreply.github.com>
2019-09-12 15:28:58 +0530

    Update Module1.vb

--- ConsoleApp12/Module1.vb
+++ ConsoleApp12/Module1.vb
@@ -5,6 +5,8 @@
         Console.WriteLine("How are you")
         Console.WriteLine("How are you")
         Console.WriteLine("Hello")
+        Console.WriteLine("Hello1")
+        Console.WriteLine("Hello2")
     End Sub
 
 End Module
