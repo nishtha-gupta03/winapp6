8aab494d23f9fd534a04a29733e8d57593ea0663
nishtha-gupta03 <54981711+nishtha-gupta03@users.noreply.github.com>
2019-09-13 16:22:34 +0530

    master changes

--- ConsoleApp12/Module1.vb
+++ ConsoleApp12/Module1.vb
@@ -1,16 +1,6 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hello")
-        Console.WriteLine("How are you")
-        Console.WriteLine("How are you")
-        Console.WriteLine("Hello")
-        Console.WriteLine("Hello1")
-        Console.WriteLine("Hello2")
-        Console.WriteLine("Hi this is akshita jaiswal")
-        Console.WriteLine("Hi this is nishtha gupta")
-        Console.WriteLine("Hi this is akshita jaiswal310")
-        Console.WriteLine("Hi this is nishtha gupta1998")
         Console.WriteLine("How r u nishtha")
         Console.WriteLine("1234")
     End Sub
